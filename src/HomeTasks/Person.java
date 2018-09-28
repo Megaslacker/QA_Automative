a274738dfd9f6687fa568f41356f23455ed9d266
Kotok_Artem <megaslacker@tut.by>
2018-09-28 13:22:05 +0300

    2 commit

---
+++ src/HomeTasks/Person.java
@@ -0,0 +1,50 @@
+package HomeTasks;
+
+/**
+ * Created by kotok_artem on 28.09.2018.
+ */
+
+public class Person {
+    // Variables
+    private String name;
+    private String surname;
+    private int age;
+    private String phone;
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    // Getters
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getPhone() {
+        return this.phone;
+
+    }
+
+}
+
--- src/HomeTasks/Task4_1_1.java
+++ src/HomeTasks/Task4_1_1.java
@@ -6,6 +6,15 @@ package HomeTasks;
 public class Task4_1_1 {
 
     public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("John");
+        person.setSurname("Smith");
+        person.setAge(18);
+        person.setPhone("+(123)-45-67-89");
+        System.out.println("person Name: "+ person.getName());
+        System.out.println("person Surname: "+ person.getSurname());
+        System.out.println("person Age: "+ person.getAge());
+        System.out.println("person phone: "+ person.getPhone());
 
 
 
