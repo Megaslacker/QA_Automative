fcba1c78916d01159e9ea5996046953a2b0c9c0a
megaslacker <megaslacker@tut.by>
2018-09-28 19:17:37 +0300

    123

--- src/ClassWork/Lesson4_1.java
+++ src/ClassWork/Lesson4_1.java
@@ -1,4 +1,24 @@
 package ClassWork;
 
-public class Lesson4_1 {
-}
+public class Class4_1 {
+
+    public static String[] toF(int[] arr){
+        String[] out = new String[arr.length];
+        for(int i =0; i<arr.length; i++){
+            if (i%2==0){
+                out[i]="F";
+            }
+            else {
+                out[i]=String.valueOf(arr[i]);
+            }
+        }
+        return out;
+    }
+
+    public static void main(String[] args) {
+
+        int [] mas = {5,10,-8,17,6};
+        String[]str=toF(mas);
+        System.out.println(str);
+
+    };
