f2ad635402f549e1c0c04ea28f91987d9a005bdf
Kotok_Artem <megaslacker@tut.by>
2018-10-01 10:16:10 +0300

    work 1

--- src/HomeTasks/ArraySorter.java
+++ src/HomeTasks/ArraySorter.java
@@ -1,11 +1,50 @@
 package HomeTasks;
-// test message
+
 public class ArraySorter {
-    public static int [] bubleSort (int[] arr){
+    public static int[] bubleSort(int[] arr) {
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int t=0; t < arr.length - 1-i; t++) {
+                if (arr[t] < arr[t + 1]) {
+                     temp = arr[t];
+                    arr[t]= arr[t + 1];
+                    arr[t + 1] = temp;
+                }
+            }
+        }
 
         return arr;
     }
-    public static int[] minMaxSorting(int[] arr){
+
+    public static int[] minMaxSorting(int[] arr) {
+        int tempMin=0;
+        int tempMax=0;
+        for (int i =0; i<arr.length; i++){
+            int max = arr[i];
+            int min = arr[i];
+            int maxPosition = i;
+            int minPosition = i;
+
+            for (int t = i; t < arr.length; t++) {
+                if (arr[t] > max) {
+                    max = arr[t]; //determine Max array value
+                    maxPosition = i; //determine Max value position
+                }
+                if (arr[t] < min) {
+                    min = arr[t]; //determine Min array value
+                    minPosition = t; //determine Min value position
+                }
+            }
+            tempMin = arr[minPosition];
+            arr[minPosition] = arr[i];
+            arr[i] = tempMin;
+
+            tempMax = arr[maxPosition];
+            arr[maxPosition] = arr[arr.length-i-1];
+            arr[i] = tempMax;
+
+
+        }
         return arr;
     }
 }
